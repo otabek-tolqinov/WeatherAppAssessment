3457f0848a690225bf960e37b17553c5ae080f3d
otabek <otabektolqinov30@gmail.com>
2024-03-16 13:47:22 +0500

    Permissions changed

--- weatherWebApp/weatherWebApp/settings.py
+++ weatherWebApp/weatherWebApp/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-
+CORS_ORIGIN_ALLOW_ALL=True
 # Application definition
 
 INSTALLED_APPS = [
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'weatherWebApp.urls'
