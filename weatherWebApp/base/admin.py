478756723c32000feffae5784537717a08eb7609
otabek <otabektolqinov30@gmail.com>
2024-03-16 12:32:45 +0500

    changes for admin panel

--- weatherWebApp/base/admin.py
+++ weatherWebApp/base/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from weatherWebApp.base.models import WeatherData
+from .models import WeatherData
 
 # Register your models here.
 admin.site.register(WeatherData)
