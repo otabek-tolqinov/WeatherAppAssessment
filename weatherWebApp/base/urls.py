e4e769ff594447451bd567615c949ddca31ea773
otabek <otabektolqinov30@gmail.com>
2024-03-17 19:35:56 +0500

    Changes near to final

--- weatherWebApp/base/urls.py
+++ weatherWebApp/base/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.send_data, name='send_data'),
+    path('', views.get_weather_by_city, name="get_weather"),
     path('get-weather-by-city/<str:city>/', views.get_weather_by_city, name='get_weather_by_city'),
 ]
