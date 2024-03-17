14c24508782edd8375f3142250346a10a1d6e771
otabek <otabektolqinov30@gmail.com>
2024-03-17 21:59:02 +0500

    Changes near to final13

--- weatherWebApp/static/script.js
+++ weatherWebApp/static/script.js
@@ -21,6 +21,7 @@ function fetchWeatherData() {
   fetch(`https://weatherappassessment.onrender.com/get-weather-by-city/Birmingham/`)
     .then(response => response.json())
     .then(response => {
+    console.log(response)
     document.getElementById("weather_state").innerHTML = response['description'];
 
 
