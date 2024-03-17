4e31d2a5ae78995e84b737ecf3b163f689af9cc2
otabek <otabektolqinov30@gmail.com>
2024-03-17 20:00:47 +0500

    Changes near to final2

--- weatherWebApp/base/views.py
+++ weatherWebApp/base/views.py
@@ -13,29 +13,6 @@ API_KEY = "ade9f4cabec74e36b585f951ace85a05"
 def get_weather_by_city(request, city=None):
     # Query the database for WeatherData objects with the given city_name
     if city:
-        if request.method == "GET":
-            city = "Birmingham"
-
-            api_url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric'
-            response = requests.get(api_url)
-
-            if response.status_code == 200:
-                weather_data = response.json()
-                description = weather_data['weather'][0]['description']
-                temperature = weather_data['main']['temp']
-                pressure = weather_data['main']['pressure']
-                icon = weather_data['weather'][0]['icon']
-
-                WeatherData.objects.create(
-                    description=description,
-                    temperature=temperature,
-                    pressure=pressure,
-                    icon=icon,
-                    city=city
-                )
-
-        return render(request,"index.html")
-    else:
         city_name = city
 
         if city_name:
@@ -58,6 +35,31 @@ def get_weather_by_city(request, city=None):
                 return JsonResponse({"error": "Weather information not found for the specified city"}, status=404)
         else:
             return JsonResponse({"error": "No city name provided"}, status=400)
+
+
+    else:
+        if request.method == "GET":
+            city = "Birmingham"
+
+            api_url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric'
+            response = requests.get(api_url)
+
+            if response.status_code == 200:
+                weather_data = response.json()
+                description = weather_data['weather'][0]['description']
+                temperature = weather_data['main']['temp']
+                pressure = weather_data['main']['pressure']
+                icon = weather_data['weather'][0]['icon']
+
+                WeatherData.objects.create(
+                    description=description,
+                    temperature=temperature,
+                    pressure=pressure,
+                    icon=icon,
+                    city=city
+                )
+
+        return render(request,"index.html")
 
 
 
