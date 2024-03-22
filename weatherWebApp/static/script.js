785e17e2aa60cc907e58fdabf4824cd0dc0bafd4
otabek <otabektolqinov30@gmail.com>
2024-03-22 12:03:44 +0500

    Changes near to final14

--- weatherWebApp/static/script.js
+++ weatherWebApp/static/script.js
@@ -8,29 +8,21 @@ let formatted_time = `${get_hours}:${get_minutes}`;
 let dateHTML = `<p>${get_month}/${get_day}/${get_year}</p>`;
 
 
-
-
-const API_KEY = "78a96a8c1b99d70f3cf7f1659c3961ca";
-
+document.getElementById("weatherForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+});
 
 function fetchWeatherData() {
-  fetch(`https://weatherappassessment.onrender.com/get-weather-by-city/Birmingham/`)
+    fetch(`https://weatherappassessment.onrender.com/get-weather-by-city/Birmingham/`)
     .then(response => response.json())
     .then(response => {
-    console.log(response)
-    document.getElementById("weather_state").innerHTML = response['description'];
-
-
-    document.getElementById("temp").innerHTML=response['temperature'];
-
-    document.getElementById("pressure").innerHTML=response['pressure'];
-    document.getElementById("icon").src = `https://openweathermap.org/img/wn/${response['icon']}@2x.png`;
-
-
+        console.log(response)
+        document.getElementById("weather_state").innerHTML = response['description'];
+        document.getElementById("temp").innerHTML=response['temperature'];
+        document.getElementById("pressure").innerHTML=response['pressure'];
+        document.getElementById("icon").src = `https://openweathermap.org/img/wn/${response['icon']}@2x.png`;
     })
     .catch(err => {
-      console.log(err);
+        console.log(err);
     });
-
-
 }
