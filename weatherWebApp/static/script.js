b2a2a01b8166fff9db1c3438f270096c002e12ff
otabek <otabektolqinov30@gmail.com>
2024-04-06 15:28:26 +0500

    caching

--- weatherWebApp/static/script.js
+++ weatherWebApp/static/script.js
@@ -1,28 +1,52 @@
-let getdate = new Date();
-let get_hours = getdate.getHours();
-let get_minutes = getdate.getMinutes();
-let get_month = getdate.getMonth() + 1;
-let get_day = getdate.getDate();
-let get_year = getdate.getFullYear();
-let formatted_time = `${get_hours}:${get_minutes}`;
-let dateHTML = `<p>${get_month}/${get_day}/${get_year}</p>`;
 
+let API_KEY = "78a96a8c1b99d70f3cf7f1659c3961ca"
 
 document.getElementById("weatherForm").addEventListener("submit", function(event) {
     event.preventDefault();
 });
 
+
+//function fetchWeatherData() {
+//    fetch(`https://weatherappassessment.onrender.com/get-weather-by-city/Birmingham/`)
+//    .then(response => response.json())
+//    .then(response => {
+//        console.log(response)
+//        document.getElementById("weather_state").innerHTML = response['description'];
+//        document.getElementById("temp").innerHTML=response['temperature'];
+//        document.getElementById("pressure").innerHTML=response['pressure'];
+//        document.getElementById("icon").src = `https://openweathermap.org/img/wn/${response['icon']}@2x.png`;
+//    })
+//    .catch(err => {
+//        console.log(err);
+//    });
+//}
+
+
+//const cacheCity=localStorage.getItem('city_name');
+//const cacheTemp=localStorage.getItem('temp');
+//const cachePressure=localStorage.getItem('pressure');
+//const cacheState=localStorage.getItem('weather_state');
+let city_name = document.getElementById("cityInput")
+
 function fetchWeatherData() {
-    fetch(`https://weatherappassessment.onrender.com/get-weather-by-city/Birmingham/`)
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_KEY}`)
     .then(response => response.json())
     .then(response => {
-        console.log(response)
-        document.getElementById("weather_state").innerHTML = response['description'];
-        document.getElementById("temp").innerHTML=response['temperature'];
-        document.getElementById("pressure").innerHTML=response['pressure'];
-        document.getElementById("icon").src = `https://openweathermap.org/img/wn/${response['icon']}@2x.png`;
+      console.log(response);
+      localStorage.setItem("city_name", JSON.stringify(city_name))
+      localStorage.setItem("temp", JSON.stringify((response.main.temp-273).toFixed(1)))
+      localStorage.setItem("pressure", JSON.stringify(city_name))
+      localStorage.setItem("weather_state", JSON.stringify(response.weather[0].description))
+      document.getElementById("weather_state").innerHTML = response.weather[0].description;
+      document.getElementById("temp").innerHTML=(response.main.temp-273).toFixed(1);
+      document.getElementById("icon").src = `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
+      document.getElementById("pressure").innerHTML = response.main.pressure;
     })
     .catch(err => {
-        console.log(err);
+      console.log(err);
     });
+
+
+
+
 }
