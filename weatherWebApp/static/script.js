d40c995f8094fb0686fd8e134cb844221859f2a4
otabek <otabektolqinov30@gmail.com>
2024-04-06 16:01:17 +0500

    change in js

--- weatherWebApp/static/script.js
+++ weatherWebApp/static/script.js
@@ -6,26 +6,7 @@ document.getElementById("weatherForm").addEventListener("submit", function(event
 });
 
 
-//function fetchWeatherData() {
-//    fetch(`https://weatherappassessment.onrender.com/get-weather-by-city/Birmingham/`)
-//    .then(response => response.json())
-//    .then(response => {
-//        console.log(response)
-//        document.getElementById("weather_state").innerHTML = response['description'];
-//        document.getElementById("temp").innerHTML=response['temperature'];
-//        document.getElementById("pressure").innerHTML=response['pressure'];
-//        document.getElementById("icon").src = `https://openweathermap.org/img/wn/${response['icon']}@2x.png`;
-//    })
-//    .catch(err => {
-//        console.log(err);
-//    });
-//}
 
-
-//const cacheCity=localStorage.getItem('city_name');
-//const cacheTemp=localStorage.getItem('temp');
-//const cachePressure=localStorage.getItem('pressure');
-//const cacheState=localStorage.getItem('weather_state');
 let city_name = document.getElementById("cityInput").value
 
 function fetchWeatherData() {
